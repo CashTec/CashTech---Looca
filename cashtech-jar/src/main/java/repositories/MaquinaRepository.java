67ac0a6887f7da5bfb5990f1b4f7e659b8eccdf6
MuriloSBarbosa <murilo.barbosa@sptech.school>
2023-04-16 14:43:59 -0300

    CREATE - inicio de cadastro automático da Maquina e processos permitidos

---
+++ cashtech-jar/src/main/java/repositories/MaquinaRepository.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package repositories;
+
+import cashtech.jar.DataBase;
+import com.github.britooo.looca.api.group.rede.RedeParametros;
+import com.github.britooo.looca.api.group.sistema.Sistema;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ * @author murilo
+ */
+public class MaquinaRepository {
+
+    DataBase conexao = new DataBase();
+    RedeParametros redeParametros;
+
+    public JdbcTemplate con = conexao.getConnection();
+
+    public void cadastrarSistema(Sistema sistema) {
+        con.update("INSERT INTO `cashtech`.`Sistema` (`id`, `nome`, `fabricante`, `arquitetura`)"
+                + " VALUES (null,?, ?, ?);",
+                sistema.getSistemaOperacional(), sistema.getFabricante(), sistema.getArquitetura());
+    }
+
+    public void cadastrarEndereco() {
+        con.update("INSERT INTO `cashtech`.`Endereco` (`id`, `rua`, `bairro`, `numero`, `cep`, `complemento`) "
+                + "VALUES (NULL, NULL, NULL, NULL, NULL, NULL)");
+    }
+
+    public Integer cadastrarMaquina(RedeParametros parametros) {
+
+        con.update("INSERT INTO `cashtech`.`CaixaEletronico` "
+                + "(`id`, `identificador`, `situacao`, `empresa_id`, `endereco_id`, `sistema_id`) "
+                + "VALUES (NULL,?,'ativo', 1, (select id from endereco order by id desc limit 1),"
+                + "(select id from sistema order by id desc limit 1));",
+                parametros.getHostName());
+
+        return 1;
+    }
+
+    public void cadastrarProcessosPermitidosPadrao(List<String> processos) {
+
+        String sql = "INSERT INTO `cashtech`.`ProcessoPermitido` (`id`, `nome`, `empresa_id`) VALUES ";
+        for (int i = 0; i < processos.size(); i++) {
+            String processo = processos.get(i);
+            if (i == processos.size() - 1) {
+                sql += String.format("(null,'%s',1)", processo);
+            } else {
+                sql += String.format("(null,'%s',1), ", processo);
+            }
+        }
+        con.update(sql);
+    }
+
+}
---
+++ cashtech-jar/src/main/java/services/CadastrarMaquina.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import cashtech.jar.DataBase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import com.github.britooo.looca.api.core.Looca;
+import com.github.britooo.looca.api.group.discos.DiscoGrupo;
+import com.github.britooo.looca.api.group.memoria.Memoria;
+import com.github.britooo.looca.api.group.processador.Processador;
+import com.github.britooo.looca.api.group.processos.Processo;
+import com.github.britooo.looca.api.group.processos.ProcessoGrupo;
+import com.github.britooo.looca.api.group.rede.Rede;
+import com.github.britooo.looca.api.group.rede.RedeInterface;
+import com.github.britooo.looca.api.group.rede.RedeInterfaceGroup;
+import com.github.britooo.looca.api.group.rede.RedeParametros;
+import com.github.britooo.looca.api.group.sistema.Sistema;
+import java.util.ArrayList;
+import java.util.List;
+import repositories.MaquinaRepository;
+
+/**
+ *
+ * @author murilo
+ */
+public class CadastrarMaquina {
+
+    Looca looca = new Looca();
+    Sistema sistema = looca.getSistema();
+    Memoria memoria = looca.getMemoria();
+    Processador processador = looca.getProcessador();
+    DiscoGrupo grupoDeDiscos = looca.getGrupoDeDiscos();
+    Rede rede = looca.getRede();
+    ProcessoGrupo grupoDeProcessos = looca.getGrupoDeProcessos();
+    RedeParametros parametros = rede.getParametros();
+    RedeInterfaceGroup redeInterfaceGroup = rede.getGrupoDeInterfaces();
+    List<RedeInterface> redeInterfaces = redeInterfaceGroup.getInterfaces();
+
+    MaquinaRepository executar = new MaquinaRepository();
+
+    public Integer executarCadastro() {
+        executar.cadastrarSistema(sistema);
+        executar.cadastrarEndereco();
+        Integer idATM = executar.cadastrarMaquina(parametros);
+
+        // =============== Cadastrar Processos permitidos ================
+        // Lista de processos permitidos são os primeiros processos que carrega
+        List<Processo> processosPermitidos = new ArrayList(grupoDeProcessos.getProcessos());
+
+        // Passar esses processos para apenas o nome
+        List<String> nomeProcessosPermitidos = new ArrayList();
+        for (Processo processo : processosPermitidos) {
+            if (!nomeProcessosPermitidos.contains(processo.getNome())) {
+                nomeProcessosPermitidos.add(processo.getNome());
+            }
+        }
+        System.out.println(nomeProcessosPermitidos);
+        executar.cadastrarProcessosPermitidosPadrao(nomeProcessosPermitidos);
+        // ==================================================================
+        return idATM;
+    }
+
+}
